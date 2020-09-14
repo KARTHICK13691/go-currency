8746397c1faf760d72893c64d8d257e240f8cccd
karthickeyan <39953593+KARTHICK13691@users.noreply.github.com>
2020-09-14 23:09:45 +0530

    Create go-currency.go

---
+++ go-currency.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/KARTICK13691/go-currency/handlers"
+	"github.com/KARTICK13691/go-currency/middlewares"
+	"github.com/KARTICK13691/go-currency/models"
+	"github.com/go-currency/mux"
+)
+
+func main() {
+	models.InitDB("postgres://user:password@localhost/gdbname?sslmode=disable")
+	router := mux.NewRouter()
+
+	// Favicon handler
+	router.HandleFunc("/favicon.ico", handlers.Favicon)
+
+	// API Endpoints
+	router.HandleFunc("/latest", middlewares.CORS(handlers.Latest))
+	router.HandleFunc("/{date}", middlewares.CORS(handlers.Historical))
+
+	fmt.Println("* Running on http://127.0.0.1:3000/")
+	http.ListenAndServe(":3000", middlewares.Log(router))
+}
